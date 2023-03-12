db7d4a982995643470d442d6c13959e5fc661003
incesamsul <incesamsul89@gmail.com>
2023-03-12 20:44:33 +0800

    fix footer

--- portfolio.php
+++ portfolio.php
@@ -165,74 +165,7 @@
 	</div>
 
 
-	<footer class="ftco-footer ftco-bg-light ftco-section mt-5 ">
-		<div class="container">
-			<div class="row mb-5">
-				<div class="col-md">
-					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">KK Enntertain</h2>
-						<p>KK Entertain adalah sebuah perusahaan event organizer yang berbasis di makassar, Indonesia.
-							Perusahaan ini berfokus pada penyelenggaraan acara-acara hiburan seperti konser musik,
-							pertunjukan seni, festival, dan acara perusahaan. </p>
-						<ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-5">
-							<li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
-							<li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md">
-					<div class="ftco-footer-widget mb-4 ml-md-5">
-						<h2 class="ftco-heading-2">Useful Links</h2>
-						<ul class="list-unstyled">
-							<li><a href="#" class="py-2 d-block">Event Organizer</a></li>
-							<li><a href="#" class="py-2 d-block">Sewa lightning makassar</a></li>
-							<li><a href="#" class="py-2 d-block">Sewa Sound makassar</a></li>
-							<li><a href="#" class="py-2 d-block">Kk entertain</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md">
-					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">Sosial Media</h2>
-						<ul class="list-unstyled">
-							<li><a href="#" class="py-2 d-block">Facebook</a></li>
-							<li><a href="#" class="py-2 d-block">Instagram</a></li>
-							<li><a href="#" class="py-2 d-block">Twitter</a></li>
-							<li><a href="#" class="py-2 d-block">Youtube</a></li>
-						</ul>
-					</div>
-				</div>
-				<div class="col-md">
-					<div class="ftco-footer-widget mb-4">
-						<h2 class="ftco-heading-2">Contact</h2>
-						<ul class="list-unstyled">
-							<li><a href="#" class="py-2 d-block">Makassar</a></li>
-							<li><a href="#" class="py-2 d-block">info@kkentertain.com</a></li>
-							<li><a href="#" class="py-2 d-block">082345678910</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-md-12 text-center">
-
-					<p>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-						Copyright &copy;
-						<script>
-							document.write(new Date().getFullYear());
-						</script> All rights reserved | made with <i class="icon-heart color-danger" aria-hidden="true"></i> by <a href="https://incesamsul.github.io">Sam</a>
-						<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-					</p>
-				</div>
-			</div>
-		</div>
-	</footer>
-
-
-
-
+	<?php include 'footer.php' ?>
 	<?php include 'js-no-scmagic.php' ?>
 
 </body>
